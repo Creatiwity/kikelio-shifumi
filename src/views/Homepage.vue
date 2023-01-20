514200f141b7ebd1cd539dc62be7d1a578ede595
victor karcher <victor.karcher@creatiwity.net>
2023-01-20 16:21:01 +0100

    Homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,7 +1,25 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
+
 <template>
-<div class="home-page">
-    <router-link to="/game"> Jouer </router-link>
-</div>
+  <div class="home-page">
+    <img src="@/assets/images/home/splashscreenLogo.png" />
+    <router-link to="/game">
+      <img src="@/assets/images/button/play.png" />
+    </router-link>
+  </div>
 </template>
+
+<style lang="scss">
+body {
+  background-image: url("@/assets/images/home/homeBg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.home-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
