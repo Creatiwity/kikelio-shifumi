2c0321831f059d93a434317e3e8d7bf6c62b3ec7
Louan <louan.bengmah@creatiwity.net>
2023-01-20 15:23:21 +0100

    init components toolbar + item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,10 +1,36 @@
 <template>
-
+    <div class="item" @touchstart="test">
+        <div class="item-icon">
+            <img v-if="props.type === 'rock'" src="~@/assets/images/weapons/weaponRock.png"/>
+            <img v-if="props.type === 'paper'" src="~@/assets/images/weapons/weaponPaper.png"/>
+            <img v-if="props.type === 'scissor'" src="~@/assets/images/weapons/weaponScissor.png"/>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-    type: 'rock' | 'paper' | 'scisors',
+    type: 'rock' | 'paper' | 'scissor',
     forPlayer: '1' | '2',
 }>()
+
+function test() {
+    console.log(props.type);
+}
 </script>
+
+<style lang="scss">
+.item {
+    width: 226px;
+    height: 226px;
+    background: url('@/assets/images/weapons/weaponBackground.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    .item-icon {
+        img {
+            width: 100%;
+        }
+    }
+}
+</style>
--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,6 +1,24 @@
 <template>
-
+    <div class="tool-bar">
+        <Item type="rock" :for-player="props.forPlayer"/>
+        <Item type="paper" :for-player="props.forPlayer"/>
+        <Item type="scissor" :for-player="props.forPlayer"/>
+    </div>
 </template>
 
 <script setup lang="ts">
+import Item from '../components/Item.vue';
+
+const props = defineProps<{
+    forPlayer: '1' | '2',
+}>()
 </script>
+
+<style scoped lang="scss">
+.tool-bar {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 24px 64px;
+}
+</style>
